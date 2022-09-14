6e5769f3054c2bd95d20bd2b4e88aef5ad44e122
SantiMoltedo <santiago.moltedo@gire.com>
2022-09-14 15:28:37 -0300

    Arreglo de navbar responsibe

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -3,21 +3,27 @@ import { Link } from 'react-router-dom'
 
 export const NavBar = () => {
     return (
-        <nav className="navbar navbar-expand-lg bg">
-            <a className="navbar-brand text-light ms-3" href="">LOGO NOMBRE DEL PROYECTO</a>
-            <ul className="navbar-nav ms-auto">
-                <li className="nav-item dropdown me-4">
-                    <a className="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <i className="fa-solid fa-user"></i> Nombre de Usuario
-                    </a>
-                    <ul className="dropdown-menu w-100">
-                        <li><Link className="dropdown-item text-light" to="/ver-perfil">Ver Perfil</Link></li>
-                        <li className='ps-3 pe-3'><hr className="dropdown-divider" /></li>
-                        <li><Link className="dropdown-item text-light" to="/logout">Cerrar Sesión</Link></li>
+        <>
+            <nav className="navbar navbar-expand-lg bg">
+                <Link className="navbar-brand text-light ms-3" to="">LOGO NOMBRE DEL PROYECTO</Link>
+                <button class="navbar-toggler me-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id='navbar'>
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle text-light ms-3" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i className="fa-solid fa-user"></i> Nombre de Usuario
+                            </a>
+                            <ul className="dropdown-menu w-100">
+                                <li><Link className="dropdown-item text-light" to="/ver-perfil">Ver Perfil</Link></li>
+                                <li className='ps-3 pe-3'><hr className="dropdown-divider" /></li>
+                                <li><Link className="dropdown-item text-light" to="/logout">Cerrar Sesión</Link></li>
+                            </ul>
+                        </li>
                     </ul>
-                </li>
-            </ul>
-
-        </nav>
+                </div>
+            </nav>
+        </>
     )
 }
